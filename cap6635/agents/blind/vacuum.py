a04fa82ae4d68b3cfef2b4e3ba6000c2128da4fb
nickumia <nickumia6@gmail.com>
2023-06-25 16:04:08 -0400

    new: model based vacuum
    - Abstract as much shared functionality to the base vacuum class
    - Move 'clean' method to base class
    - the model-based vacuum uses the dimensions of the environment to know how to move, but it does not search for dirt, so it's not informed... it just adjusts it's motion based on the environment layout, not dirt

--- cap6635/agents/blind/vacuum.py
+++ cap6635/agents/blind/vacuum.py
@@ -8,6 +8,8 @@ class Vacuum:
         self._e = environ
         self._x = start[0]
         self._y = start[1]
+        self._x_path = []
+        self._y_path = []
         self._utility = 0
         self._time = 0
 
@@ -27,32 +29,10 @@ class Vacuum:
     def time(self, val):
         self._time += val
 
-
-class SimpleVacuum(Vacuum):
-
-    def __init__(self, environ):
-        super(SimpleVacuum, self).__init__(environ)
-        self._x_path = []
-        self._y_path = []
-
-    def chooseMove(self):
-        if self._e.map[self._x][self._y] == 2:
-            return 4
-
-        actions = [0, 1, 2, 3, 6]
-        if self._x == 1:
-            actions.remove(0)
-        if self._x == self._e._x-2:
-            actions.remove(1)
-        if self._y == 1:
-            actions.remove(2)
-        if self._y == self._e._y-2:
-            actions.remove(3)
-
-        return random.choice(actions)
-
     def move(self):
-        action = self.chooseMove()
+        action = self.clean()
+        if not action:
+            action = self.chooseMove()
         if (action == 0):
             print("up")
             self._x -= 1
@@ -80,6 +60,10 @@ class SimpleVacuum(Vacuum):
         self.add_to_path((self._x, self._y))
         self.time = 1
 
+    def clean(self):
+        if self._e.map[self._x][self._y] == 2:
+            return 4
+
     @property
     def x_path(self):
         return self._x_path
@@ -91,3 +75,48 @@ class SimpleVacuum(Vacuum):
     def add_to_path(self, point):
         self._x_path.append(point[0])
         self._y_path.append(point[1])
+
+
+class SimpleVacuum(Vacuum):
+
+    def chooseMove(self):
+        actions = [0, 1, 2, 3, 6]
+        if self._x == 1:
+            actions.remove(0)
+        if self._x == self._e._x-2:
+            actions.remove(1)
+        if self._y == 1:
+            actions.remove(2)
+        if self._y == self._e._y-2:
+            actions.remove(3)
+
+        return random.choice(actions)
+
+
+class ModelVacuum(Vacuum):
+
+    def chooseMove(self):
+        # last column
+        if self._y == self._e._y-2:
+            # even # of columns
+            if self._e._x % 2 == 0:
+                # stop at the top
+                if self._x == 1:
+                    return 5
+            else:
+                # stop at the bottom
+                if self._x == self._e._x-2:
+                    return 5
+        # odd columns, move down
+        if self._y % 2 == 1:
+            # bottom tile, move right
+            if self._x == self._e._x-2:
+                return 3
+            return 1
+        # even columns, move up
+        if self._y % 2 == 0:
+            # top tile, move right
+            if self._x == 1:
+                return 3
+            return 0
+
--- vacuums.py
+++ vacuums.py
@@ -1,10 +1,11 @@
 
-from cap6635.agents.blind.vacuum import SimpleVacuum
+from cap6635.agents.blind.vacuum import SimpleVacuum, ModelVacuum
 from cap6635.environment.map import Carpet
 
 
 world = Carpet(6,6)
-agent = SimpleVacuum(world)
+# agent = SimpleVacuum(world)
+agent = ModelVacuum(world)
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -16,7 +17,6 @@ while world.dirtPresent():
     plt.imshow(world.map, 'pink')
     plt.show(block=False)
     plt.plot(agent.y_path,agent.x_path,'r:',linewidth=1)
-    print(agent.y_path,agent.x_path)
     if len(agent.x_path) > 0:
         plt.plot(agent.y_path[-1], agent.x_path[-1], '*r', 'Robot field', 5)
     plt.pause(0.5)
