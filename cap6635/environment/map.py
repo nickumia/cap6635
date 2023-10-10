49c03f02ba810070d7d0daf074584fc085031e8a
nickumia <nickumia6@gmail.com>
2023-10-09 22:49:27 -0400

    Proper Tic-Tac-Toe 🤦‍♀️  (#15)
    * new: tictactoe baseline
    
    - Tictactoe board (WITH PROPER WIN CALCULATIONS -_-)
    - Base minimax class with single player (just human being x)
    - Auto complete tictactoe game, it plays randomly, so no guaranteed win
    
    * new: allow player to be X or O
    
    - This was sooooo agonizing :'''''(
    - Define root player
    - Define a 'turn' variable to say who's playing
    - If player is X, max is O.  If player is O, max is X.
    - If player is X, min is X.  If player is O, min is O.
    - Define game logic for who plays when.
    
    * cleanup: unnecessary variable
    
    * new: alpha-beta pruning
    
    - Basic support for alpha beta pruning
    
    * fix: make algo compatible for normal + alpha-beta
    
    * new: add tests for tictactoe
    
    * lint: blah
    
    * refactor: consolidate code
    
    Print winner at the end

--- cap6635/environment/map.py
+++ cap6635/environment/map.py
@@ -3,16 +3,18 @@ import numpy as np
 import random
 
 from cap6635.utilities.constants import (
-    STATE_CLEAN, STATE_DIRTY, STATE_OBSTACLE
+    STATE_CLEAN, STATE_DIRTY, STATE_OBSTACLE,
+    TTT_NONE, TTT_X, TTT_O
 )
 
 
 class Map2D:
-    def __init__(self, m=10, n=10):
+    def __init__(self, m=10, n=10, wall=True):
         self._x = m
         self._y = n
         self._map = np.zeros((m, n))
-        self.buildWall()
+        if wall:
+            self.buildWall()
 
     def buildWall(self):
         self._map[:, 0] = STATE_OBSTACLE
@@ -47,3 +49,75 @@ class Carpet(Map2D):
 
     def dirtPresent(self):
         return STATE_DIRTY in self._map
+
+
+class TicTacToe(Map2D):
+
+    def __init__(self, x=3, y=3):
+        super(TicTacToe, self).__init__(x, y, wall=False)
+        self._win = False
+
+    @property
+    def win(self):
+        return self._win
+
+    @win.setter
+    def win(self, v):
+        self._win = v
+
+    def is_empty(self, x, y):
+        if self.map[x, y] == TTT_NONE:
+            return True
+        return False
+
+    def is_valid(self, x, y):
+        if x >= 0 and x < self._x:
+            if y >= 0 and y < self._y:
+                if self.map[x, y] == TTT_NONE:
+                    return True
+        return False
+
+    def is_win(self):
+        # Horizontal win
+        for x in range(0, self._x):
+            if (self.map[x] == [self.map[x][0]] * self._x).all() and \
+               self.map[x][0] != TTT_NONE:
+                self.win = self.map[x][0]
+                return True
+
+        # Vertical win
+        # TODO: make this work for more than 3
+        for y in range(0, self._y):
+            if self.map[0][y] != TTT_NONE and \
+               self.map[0][y] == self.map[1][y] == self.map[2][y]:
+                self.win = self.map[0][y]
+                return True
+
+        # Diagonal win
+        # TODO: make this work for more than 3
+        if self.map[1][1] != TTT_NONE:
+            if self.map[0][0] == self.map[1][1] == self.map[2][2] or \
+               self.map[0][2] == self.map[1][1] == self.map[2][0]:
+                self.win = self.map[1][1]
+                return True
+
+        # Full board
+        if TTT_NONE not in self.map:
+            self.win = TTT_NONE
+            return True
+
+        return False
+
+    def print_board(self):
+        for i in range(0, self._x):
+            for j in range(0, self._y):
+                char = ' '
+                if self.map[i][j] == TTT_X:
+                    char = 'X'
+                elif self.map[i][j] == TTT_O:
+                    char = 'O'
+                elif self.map[i][j] == TTT_NONE:
+                    char = '.'
+                print('{}|'.format(char), end=" ")
+            print()
+        print()
